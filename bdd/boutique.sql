b80258d690c0435f800f3833c32fa63535899613
Belghomari Abdelmalek <belghomariabdelmalek@gmail.com>
2024-03-30 23:50:11 +0100

    bdd changed to fit the code

--- bdd/boutique.sql
+++ bdd/boutique.sql
@@ -1,4 +1,3 @@
--- Création de la table clients
 CREATE TABLE clients (
     client_id SERIAL PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
@@ -10,26 +9,21 @@ CREATE TABLE clients (
     date_inscription DATE NOT NULL
 );
 
--- Création de la table panier
--- Supposition : code_exemplaire fait référence à un identifiant d'un article dans une autre table non définie ici
+
 CREATE TABLE panier (
     client_id INT,
     code_exemplaire INT,
     quantite INT NOT NULL,
     PRIMARY KEY (client_id, code_exemplaire),
     FOREIGN KEY (client_id) REFERENCES clients(client_id)
-    -- Vous pourriez également avoir besoin d'une clé étrangère pour code_exemplaire si cette table existe
 );
 
--- Création de la table commande
 CREATE TABLE commande (
-    commande_id SERIAL PRIMARY KEY,
     client_id INT,
     code_exemplaire INT,
     quantite INT NOT NULL,
     prix DECIMAL(10, 2) NOT NULL,
     date DATE NOT NULL,
     FOREIGN KEY (client_id) REFERENCES clients(client_id)
-    -- Encore une fois, une clé étrangère pour code_exemplaire pourrait être nécessaire
 );
 
